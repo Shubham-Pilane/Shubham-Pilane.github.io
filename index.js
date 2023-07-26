2b3de08447d93e780405494acc8ddab2af58ed3b
Shubham <shubhampilane143@gmail.com>
2023-07-26 14:24:22 +0530

    Updated portfolio

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ menu.onclick = () => {
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
+
+function downloadAndOpenResume() {
+  // Get the resume link
+  var resumeLink = document.getElementById('resume-link-2');
+
+  // Simulate a click on the link to start the download
+  var event = new MouseEvent('click');
+  resumeLink.dispatchEvent(event);
+
+  // Open the resume in a new tab
+  window.open(resumeLink.href, '_blank');
+}
      
 // document.getElementById('resume-button-1').addEventListener("click", () => {
 //   window.open("https://drive.google.com/drive/u/0/folders/19AE6HcipcqqVK-BWtjCQw6ohcZGCvKpm", "_blank");
